22e0cd5efe725bc61207eac7499058b04f9b0ba0
Adrien Fichet <adrien.fichet@gmail.com>
2023-05-09 10:56:55 +0200

    Add sendgrid comment

--- sendgrid.py
+++ sendgrid.py
@@ -1,1 +1,2 @@
+# https://github.com/ateventhorizon/event_horizon/blob/9b78c9318e1a785384ab01eb4d90e79f0192c6ad/auth-api/def_env.cmd#L8
 TOKEN="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"
