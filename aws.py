f233a06485dd8fc38c7567734f6fbe4e15bd46d1
Adrien Fichet <adrien.fichet@gmail.com>
2023-05-09 13:04:44 +0200

    Add aws.py

---
+++ aws.py
@@ -0,0 +1,12 @@
+# https://github.com/JetBrains/ring-ui/blob/ce06c7c8141602d69dbefdcd7fab9e7e7dbdbe68/.teamcity/settings.kts#LL219C5-L225C1
+params = {
+    "env.AWS_SECRET_ACCESS_KEY": "credentialsJSON:dbeb62cd-b1ba-452b-aa7c-317f19a10804",
+    "env.AWS_ACCESS_KEY_ID": "AKIAJ42U3JYQAGGFJ5KQ",
+    "env.AWS_DEFAULT_REGION": "us-east-2",
+    "env.NODE_OPTIONS": "--max-old-space-size=8192",
+}
+
+# https://github.com/MicahZoltu/uniswap-frontend/blob/e52368abff2609bcd4ac542ec251e94c2ba46cf7/.env#LL4C2-L4C2
+REACT_APP_AWS_API_ACCESS_KEY="AKIAYJJWW6AQ47ODATHN"
+REACT_APP_AWS_API_ACCESS_SECRET="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"
+REACT_APP_AWS_X_API_KEY="z9dReS5UtHu7iTrUsTuWRozLthi3AxOZlvobrIdr14"
